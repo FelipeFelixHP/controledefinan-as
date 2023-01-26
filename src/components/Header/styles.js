a6794764343f251c7417624db1a3b46c06cf43a2
Felipe Felix <ffelix035@gmail.com>
2023-01-26 00:58:07 -0300

    primeiro commit

---
+++ src/components/Header/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 170px;
+  text-align: center;
+  background: indigo;
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 20px;
+  color: #fff;
+`;
